324049406bdea0796d8f4ae2f431f9b9fc8dc03b
swarnaprava-06032005 <150228737+swarnaprava-06032005@users.noreply.github.com>
2026-02-25 12:42:25 +0530

    push Instructor page, course management page

--- AdminPannel/src/App.tsx
+++ AdminPannel/src/App.tsx
@@ -105,6 +105,8 @@ export default function App() {
           <Route path="/course/manage" element={<CourseManage />} />
           <Route path="/course/approval" element={<CourseApproval/>} />
 
+          {/* ================= COURSE ================= */}
+
           {/* ================= Video Management ================= */}
           <Route path="/gallery/videos" element={<VideoManagement/>}/>
            
